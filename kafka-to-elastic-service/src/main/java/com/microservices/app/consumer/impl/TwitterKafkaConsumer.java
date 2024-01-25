d8ade58c5e6db0acc5a9c59ee17579c86f2fbdaa
tunt99 <nguyentuantu2101@gmail.com>
2024-01-25 11:07:22 +0700

    Twitter topic listener: Add initialization check

--- kafka-to-elastic-service/src/main/java/com/microservices/app/consumer/impl/TwitterKafkaConsumer.java
+++ kafka-to-elastic-service/src/main/java/com/microservices/app/consumer/impl/TwitterKafkaConsumer.java
@@ -5,8 +5,11 @@ import com.microservices.app.consumer.KafkaConsumer;
 import com.microservices.app.kafka.admin.client.KafkaAdminClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -21,8 +24,19 @@ public class TwitterKafkaConsumer implements KafkaConsumer {
     private final KafkaAdminClient kafkaAdminClient;
     private final KafkaConfigData kafkaConfigData;
 
+    @EventListener
+    public void onAppStarted(ApplicationStartedEvent event) {
+        kafkaAdminClient.checkTopicsCreated();
+        log.info("Topics with name {} is ready for operations!", kafkaConfigData.getTopicNamesToCreate().toArray());
+        MessageListenerContainer messageListenerContainer =
+                kafkaListenerEndpointRegistry.getListenerContainer("twitterTopicListener");
+        if (messageListenerContainer != null){
+            messageListenerContainer.start();
+        }
+    }
+
     @Override
-    @KafkaListener(topics = "${kafka-config.topic-name}", autoStartup = "true")
+    @KafkaListener(id = "twitterTopicListener", topics = "${kafka-config.topic-name}")
     public void receive(@Payload List<String> message) {
         log.info("KAFKA CONSUME: {}", message);
     }
