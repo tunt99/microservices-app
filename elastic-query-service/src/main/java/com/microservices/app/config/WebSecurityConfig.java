c57b27c7903c1d470c5f01feceeb924efeedb7d3
tunt99 <nguyentuantu2101@gmail.com>
2024-02-28 14:31:37 +0700

    Config Spring Security Oauth 2.0, OpenID Connect and JWT

--- elastic-query-service/src/main/java/com/microservices/app/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/app/config/WebSecurityConfig.java
@@ -1,19 +1,36 @@
 package com.microservices.app.config;
 
+import com.microservices.app.security.TwitterQueryUserDetailsService;
+import com.microservices.app.security.TwitterQueryUserJwtConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class WebSecurityConfig {
+
+    private final TwitterQueryUserDetailsService twitterQueryUserDetailsService;
+
+    private final OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
 
     @Value("${security.paths-to-ignore}")
     private String[] pathsToIgnore;
@@ -23,12 +40,36 @@ public class WebSecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
-                                .requestMatchers("/**").permitAll());
+                                .anyRequest().fullyAuthenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer((oauth2) ->
+                        oauth2.jwt(jwtConfigurer ->
+                                jwtConfigurer.jwtAuthenticationConverter(twitterQueryUserJwtConverter())));
         return http.build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers(pathsToIgnore);
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(@Qualifier("elastic-query-service-audience-validator")
+                                  OAuth2TokenValidator<Jwt> audienceValidator) {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(
+                oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+        OAuth2TokenValidator<Jwt> withIssuer =
+                JwtValidators.createDefaultWithIssuer(
+                        oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+        OAuth2TokenValidator<Jwt> withAudience =
+                new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+        jwtDecoder.setJwtValidator(withAudience);
+        return jwtDecoder;
+    }
+
+    @Bean
+    Converter<Jwt, ? extends AbstractAuthenticationToken> twitterQueryUserJwtConverter() {
+        return new TwitterQueryUserJwtConverter(twitterQueryUserDetailsService);
     }
 }
---
+++ elastic-query-service/src/main/java/com/microservices/app/constant/Constants.java
@@ -0,0 +1,5 @@
+package com.microservices.app.constant;
+
+public class Constants {
+    public static final String NA = "N/A";
+}
